2640b413cfa5ad77a9927c995bf2aac59b503e53
locnd7 <locnd@eupgroup.net>
2026-01-08 14:32:31 +0700

    .

--- requirements.sh
+++
@@ -1,5 +0,0 @@
-pip install torch torchvision torchaudio xformers --index-url https://download.pytorch.org/whl/cu124
-pip install datasets==4.3.0 sentencepiece protobuf huggingface_hub hf_transfer
-pip install bitsandbytes accelerate peft trl triton cut_cross_entropy unsloth_zoo
-pip install unsloth
-pip install tensorboardX
---
+++ src/train/requirements.sh
@@ -0,0 +1,5 @@
+pip3 install torch torchvision torchaudio xformers --index-url https://download.pytorch.org/whl/cu124
+pip3 install datasets==4.3.0 sentencepiece protobuf huggingface_hub hf_transfer
+pip3 install bitsandbytes accelerate peft trl triton cut_cross_entropy unsloth_zoo
+pip3 install unsloth
+pip3 install tensorboardX
