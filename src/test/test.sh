1cd0c2ed48265ddae06f77234669aef54f7550ae
locnd7 <locnd@eupgroup.net>
2026-01-08 14:54:27 +0700

    .

--- src/test/test.sh
+++ src/test/test.sh
@@ -4,6 +4,6 @@ curl -X POST "http://localhost:8888/v1/translate" \
   -H "Content-Type: application/json" \
   -d '{
     "text": "まずはイタリアの計画について説明しましょう。イタリアは主にドイツと日本の「弟分」でした。",
-    "target_language_code": "ベトナム語",
+    "target_language_code": "Vietnamese",
     "temperature": 0.20
   }'
