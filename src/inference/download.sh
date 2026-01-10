5d0c52721c69b02796a56ddf1da8830d6ac262ca
locnd7 <locnd@eupgroup.net>
2026-01-10 22:36:23 +0700

    .

--- src/inference/download.sh
+++ src/inference/download.sh
@@ -5,6 +5,9 @@ PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../" && pwd)"
 # HF config
 export HF_HOME="$PROJECT_ROOT/model"
 
+# Base model
 # hf download Qwen/Qwen3-8B
-hf download Qwen/Qwen3-4B-Thinking-2507
+hf download deepseek-ai/DeepSeek-R1-0528-Qwen3-8B
+
+# Adapter
 # hf download bunbohue/Mazii-MT-10-06-01_07-01-2026
