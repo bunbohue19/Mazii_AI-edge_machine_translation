1fcd959fd1a5caef06250beb1ce5a48043e05342
locnd7 <locnd@eupgroup.net>
2026-01-14 14:15:12 +0700

    .

--- src/inference/download.sh
+++ src/inference/download.sh
@@ -11,17 +11,4 @@ hf download Qwen/Qwen3-4B
 
 # Adapter
 hf download bunbohue/Mazii-MT-09-38-43_13-01-2026
-hf download bunbohue/Mazii-MT-10-25-26_13-01-2026PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../" && pwd)"
-
-# echo $PROJECT_ROOT
-
-# HF config
-export HF_HOME="$PROJECT_ROOT/model"
-
-# Base model
-hf download Qwen/Qwen3-8B
-hf download Qwen/Qwen3-4B
-
-# Adapter
-hf download bunbohue/Mazii-MT-09-38-43_13-01-2026
 hf download bunbohue/Mazii-MT-10-25-26_13-01-2026
